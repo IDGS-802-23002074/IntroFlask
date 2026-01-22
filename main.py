d1a35787c22816839ede3a6471f69c5099b043f5
IDGS-802-23002074 <arlethmoran1720@gmail.com>
2026-01-21 21:03:21 -0600

    operasBas

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     titulo="IDGS-802-flask"
     lista=['Juan','Karla','Miguel','Ana']
-return render_template('index.html',titulo=titulo,lista=lista)
+    return render_template('index.html',titulo=titulo,lista=lista)
 
 @app.route('/formularios')
 def formularios():
-return render_template('formulario.html')
+    return render_template('formulario.html')
 
 @app.route('/reportes')
 def reportes():
-return render_template('reportes.html')
+    return render_template('reportes.html')
 
 @app.route('/hola')
 def hola():
-return "Hola, Hola"
+    return "Hola, Hola"
 
 @app.route('/user/<string:user>')
 def user(user):
-return "Hola, {user}"
+    return "Hola, {user}"
 
 @app.route('/numero/<int:n>')
 def numero(n):
-return "Numero: {}".format(n)
+    return "Numero: {}".format(n)
 
 @app.route('/user/<int:id>/<string:username>')
 def username(id,username):
-return "ID: {} nombre: {}".format(id,username)
+    return "ID: {} nombre: {}".format(id,username)
 
 @app.route('/suma/<float:n1>/<float:n2>')
 def func(n1,n2):
-return "La suma es: {}".format(n1+n2)
+    return "La suma es: {}".format(n1+n2)
 
 @app.route('/default/')
 @app.route('/default/<string:param>')
 def func2(param="juan"):
-return "Hola, {param}"
+    return "Hola, {param}"
 
 @app.route('/operas')
 def operas():
-return """
+    return """
      <form>
      <label for="name">Name:</label>
      <input type="text" id="name" name="name" required>
@@ -52,5 +52,35 @@ return """
      </form>
      """
 
+@app.route("/operasBas")
+def operas1():
+    return render_template("operasBas.html")
+
+@app.route("/resultado", methods=["POST"])
+def resultado():
+    n1 = float(request.form.get("n1"))
+    n2 = float(request.form.get("n2"))
+    operacion = request.form.get("operacion")
+
+    if operacion == "suma":
+        res = n1 + n2
+        op = "Suma"
+    elif operacion == "resta":
+        res = n1 - n2
+        op = "Resta"
+    elif operacion == "multiplicacion":
+        res = n1 * n2
+        op = "Multiplicación"
+    elif operacion == "division":
+        if n2 == 0:
+            return "No se puede dividir entre cero"
+        res = n1 / n2
+        op = "División"
+    else:
+        return "Operación no válida"
+
+    return f"{op}: {res}"
+
+
 if __name__ == '__main__':
-app.run(debug=True)
+    app.run(debug=True)
